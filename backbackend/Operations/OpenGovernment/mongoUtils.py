c9bbe633ef515e24fb9e21b27cde53528f7e84f9
Daniel Yamins <dyamins@daniel-yaminss-macbook-pro.local>
2010-04-30 17:13:05 -0400

    detabing

--- backbackend/Operations/OpenGovernment/mongoUtils.py
+++ backbackend/Operations/OpenGovernment/mongoUtils.py
@@ -18,142 +18,142 @@ def createCollection(path,certpath):
 
     """
 
-	path += '/' if path[-1] != '/' else ''	
+    path += '/' if path[-1] != '/' else ''  
 
-	#loads metadata
-	M = pickle.load(open(path + '__metadata.pickle'))
-	
-	assert 'Subcollections' in M.keys(), 'No subcollection metadata found, aborting'
-	assert '' in M['Subcollections'].keys(), 'No whole-collection metadata found, aborting'
-	AllMeta = M['Subcollections']['']
-	
-	assert 'VARIABLES' in AllMeta.keys(), 'No variable information provided, aborting.'
-	Variables = AllMeta['VARIABLES']
-	VarMap = dict(zip(Variables,[str(x) for x in range(len(Variables))]))
+    #loads metadata
+    M = pickle.load(open(path + '__metadata.pickle'))
+    
+    assert 'Subcollections' in M.keys(), 'No subcollection metadata found, aborting'
+    assert '' in M['Subcollections'].keys(), 'No whole-collection metadata found, aborting'
+    AllMeta = M['Subcollections']['']
+    
+    assert 'VARIABLES' in AllMeta.keys(), 'No variable information provided, aborting.'
+    Variables = AllMeta['VARIABLES']
+    VarMap = dict(zip(Variables,[str(x) for x in range(len(Variables))]))
 
     #establishes conneciton to "govdata" database
-	connection =  pm.Connection()
-	db = connection['govdata']
-	collectionName = path.split('/')[-2]
-	assert not collectionName.startswith('_'), 'Collection name must not start with "_", aborting.'
-	metaCollectionName = '__' + collectionName + '__'
-	collection = db[collectionName]
-	metacollection = db[metaCollectionName]
-	cleanCollection(collection)
-	cleanCollection(metacollection)
-	
-	#DEAL WITH TIME COLUMN TRANSLATION in COLUMN NAMES
-	if 'DateFormat' in AllMeta.keys():
-		TimeFormatter = td.mongotimeformatter(AllMeta['DateFormat'])
+    connection =  pm.Connection()
+    db = connection['govdata']
+    collectionName = path.split('/')[-2]
+    assert not collectionName.startswith('_'), 'Collection name must not start with "_", aborting.'
+    metaCollectionName = '__' + collectionName + '__'
+    collection = db[collectionName]
+    metacollection = db[metaCollectionName]
+    cleanCollection(collection)
+    cleanCollection(metacollection)
+    
+    #DEAL WITH TIME COLUMN TRANSLATION in COLUMN NAMES
+    if 'DateFormat' in AllMeta.keys():
+        TimeFormatter = td.mongotimeformatter(AllMeta['DateFormat'])
 
-	
-	#ADD SUBCOLLECTION DOCUMENTS --  with id = '__' + name
-	AllMeta['Source'] = pm.son.SON(AllMeta['Source'])
-	for k in M['Subcollections'].keys():
-		x = M['Subcollections'][k]
-		x['_id'] = k
-		id = metacollection.insert(x,safe=True)
-		
-	
-	#SET UP INDEXES
-	IndexCols = ['Subcollections']
-	if 'ColumnGroups' in AllMeta.keys():
-		for k in ['IndexColumns','LabelColumns','TimeColumns','SpaceColumns']:
-			if k in AllMeta['ColumnGroups'].keys():
-				IndexCols += AllMeta['ColumnGroups'][k]
-	for col in IndexCols:
-		if col in VarMap.keys():
-			collection.ensure_index(VarMap[col])
+    
+    #ADD SUBCOLLECTION DOCUMENTS --  with id = '__' + name
+    AllMeta['Source'] = pm.son.SON(AllMeta['Source'])
+    for k in M['Subcollections'].keys():
+        x = M['Subcollections'][k]
+        x['_id'] = k
+        id = metacollection.insert(x,safe=True)
+        
+    
+    #SET UP INDEXES
+    IndexCols = ['Subcollections']
+    if 'ColumnGroups' in AllMeta.keys():
+        for k in ['IndexColumns','LabelColumns','TimeColumns','SpaceColumns']:
+            if k in AllMeta['ColumnGroups'].keys():
+                IndexCols += AllMeta['ColumnGroups'][k]
+    for col in IndexCols:
+        if col in VarMap.keys():
+            collection.ensure_index(VarMap[col])
 
-	if 'UniqueIndexes' in AllMeta.keys():
-		for colset in AllMeta['UniqueIndexes']:	
-			cols = zip([VarMap[c] for c in colset],[pm.DESCENDING]*len(colset))
-			collection.ensure_index(cols,unique=True,dropDups=True)
-		
-	
-	#ADD ASSOCIATED FILES TO GRIDFS
-	if IsFile(path + '__files.pickle'):
-		Files = pickle.load(open(path + '__files.pickle'))
-		G = gfs.GridFS(db,collection=collectionName)
-		for F in Files:
-			os.environ['PROTECTION'] = 'OFF'
-			S = open(F['path'],'r').read()
-			os.environ['PROTECTION'] = 'ON'
-			G.put(S)
-	
-	#ADD RECORDS FROM CHUNKS -- CHECKING HASHES AS WE GO
-	if 'Subcollections' in VarMap.keys():
-		sc = VarMap['Subcollections']
-	else:
-		sc = None
-	if 'TimeColumns' in AllMeta['ColumnGroups'].keys():
-		tcs = [VarMap[tc] for tc in AllMeta['ColumnGroups']['TimeColumns']]
-	else:
-		tcs = []
-	
-	if 'SpaceColumns' in AllMeta['ColumnGroups'].keys():
-		spcs = [VarMap[spc] for spc in AllMeta['ColumnGroups']['SpaceColumns']]
-	else:
-		spcs = []
-		
-	SpaceCache = {}	
-	for k in M['Hashes'].keys():
-		print 'Adding chunk', k
-		hash = M['Hashes'][k]
-		poss = [x for x in listdir(path) if x.startswith(str(k) + '.')]
-		assert len(poss) == 1, 'Identification of chunk file ' + str(k) + ' in directory ' + path + ' failed, aborting.'
-		fpath = poss[0]		
-		#		assert hash == hashlib.sha1(str(pickle.load(open(path + fpath)))).hexdigest(), 'Hash of chunk file ' + str(k) + ' in directory ' + path + ' is incorrect, aborting.'
-	    
-		if fpath.endswith(('.csv','.tsv')): #handles situation when source text file is csv
+    if 'UniqueIndexes' in AllMeta.keys():
+        for colset in AllMeta['UniqueIndexes']: 
+            cols = zip([VarMap[c] for c in colset],[pm.DESCENDING]*len(colset))
+            collection.ensure_index(cols,unique=True,dropDups=True)
+        
+    
+    #ADD ASSOCIATED FILES TO GRIDFS
+    if IsFile(path + '__files.pickle'):
+        Files = pickle.load(open(path + '__files.pickle'))
+        G = gfs.GridFS(db,collection=collectionName)
+        for F in Files:
+            os.environ['PROTECTION'] = 'OFF'
+            S = open(F['path'],'r').read()
+            os.environ['PROTECTION'] = 'ON'
+            G.put(S)
+    
+    #ADD RECORDS FROM CHUNKS -- CHECKING HASHES AS WE GO
+    if 'Subcollections' in VarMap.keys():
+        sc = VarMap['Subcollections']
+    else:
+        sc = None
+    if 'TimeColumns' in AllMeta['ColumnGroups'].keys():
+        tcs = [VarMap[tc] for tc in AllMeta['ColumnGroups']['TimeColumns']]
+    else:
+        tcs = []
+    
+    if 'SpaceColumns' in AllMeta['ColumnGroups'].keys():
+        spcs = [VarMap[spc] for spc in AllMeta['ColumnGroups']['SpaceColumns']]
+    else:
+        spcs = []
+        
+    SpaceCache = {} 
+    for k in M['Hashes'].keys():
+        print 'Adding chunk', k
+        hash = M['Hashes'][k]
+        poss = [x for x in listdir(path) if x.startswith(str(k) + '.')]
+        assert len(poss) == 1, 'Identification of chunk file ' + str(k) + ' in directory ' + path + ' failed, aborting.'
+        fpath = poss[0]     
+        #       assert hash == hashlib.sha1(str(pickle.load(open(path + fpath)))).hexdigest(), 'Hash of chunk file ' + str(k) + ' in directory ' + path + ' is incorrect, aborting.'
+        
+        if fpath.endswith(('.csv','.tsv')): #handles situation when source text file is csv
 
-			X = tb.tabarray(SVfile = path + fpath,verbosity = 0)
-			names = X.dtype.names
-			for x in X:
-				newx = [float(xx) if isinstance(xx,float) else int(xx) if isinstance(xx,int) else xx for xx in x]		
-				if sc in X.dtype.names:
-					sci = X.dtype.names.index(sc)
-					newx[sci] = newx[sci].split(',')
-				for tc in tcs: #handling time formatting
-					if tc in X.dtype.names:
-						tci = X.dtype.names.index(tc)
-						newx[tci] = TimeFormatter(newx[tci])
-				for spc in spcs:  #space formatting and completiong
-					if spc in X.dtype.names:
-						spci = X.dtype.names.index(spc)
-						newx[spci] = eval(newx[spci])
-						t = getT(newx[spci])
-						if t in SpaceCache.keys():
-							newx[spci] = SpaceCache[t].copy()
-						else:
-							newx[spci] = loc.SpaceComplete(newx[spci])
-							SpaceCache[t] = newx[spci].copy()
-						#TODO: geocoding would be added here 
+            X = tb.tabarray(SVfile = path + fpath,verbosity = 0)
+            names = X.dtype.names
+            for x in X:
+                newx = [float(xx) if isinstance(xx,float) else int(xx) if isinstance(xx,int) else xx for xx in x]       
+                if sc in X.dtype.names:
+                    sci = X.dtype.names.index(sc)
+                    newx[sci] = newx[sci].split(',')
+                for tc in tcs: #handling time formatting
+                    if tc in X.dtype.names:
+                        tci = X.dtype.names.index(tc)
+                        newx[tci] = TimeFormatter(newx[tci])
+                for spc in spcs:  #space formatting and completiong
+                    if spc in X.dtype.names:
+                        spci = X.dtype.names.index(spc)
+                        newx[spci] = eval(newx[spci])
+                        t = getT(newx[spci])
+                        if t in SpaceCache.keys():
+                            newx[spci] = SpaceCache[t].copy()
+                        else:
+                            newx[spci] = loc.SpaceComplete(newx[spci])
+                            SpaceCache[t] = newx[spci].copy()
+                        #TODO: geocoding would be added here 
 
-				collection.insert(dict(zip(names,newx)))
-				
-		elif fpath.endswith('.pickle'):   #this is used when the source data is a pickle, containing dictionaries of things to be added as mongo documents
-			Chunk = pickle.load(open(path +  fpath))
-			for c in Chunk:
-				for tc in tcs:   #time handling 
-					if tc in c.keys():
-						c[tc] = TimeFormatter(c[tc])
-				for spc in spcs:
-					if spc in c.keys():   #space
-						t = getT(c[spc])
-						if t in SpaceCache.keys():
-							c[spc] = SpaceCache[t].copy()
-						else:
-							c[spc] = loc.SpaceComplete(c[spc])
-							SpaceCache[t] = c[spc].copy()
+                collection.insert(dict(zip(names,newx)))
+                
+        elif fpath.endswith('.pickle'):   #this is used when the source data is a pickle, containing dictionaries of things to be added as mongo documents
+            Chunk = pickle.load(open(path +  fpath))
+            for c in Chunk:
+                for tc in tcs:   #time handling 
+                    if tc in c.keys():
+                        c[tc] = TimeFormatter(c[tc])
+                for spc in spcs:
+                    if spc in c.keys():   #space
+                        t = getT(c[spc])
+                        if t in SpaceCache.keys():
+                            c[spc] = SpaceCache[t].copy()
+                        else:
+                            c[spc] = loc.SpaceComplete(c[spc])
+                            SpaceCache[t] = c[spc].copy()
                 #TODO:  geocoding also inserted here 
-				
-				collection.insert(c)
-		else:
-			print 'Type of chunk file', fpath, 'not recognized.' 
-		
-	connection.disconnect()
-	createCertificate(certpath,'Collection ' + collectionName + ' written to DB.')
-	
+                
+                collection.insert(c)
+        else:
+            print 'Type of chunk file', fpath, 'not recognized.' 
+        
+    connection.disconnect()
+    createCertificate(certpath,'Collection ' + collectionName + ' written to DB.')
+    
 def getT(x):
-	return tuple([(k,v) for (k,v) in x.items() if k != 'f'])
+    return tuple([(k,v) for (k,v) in x.items() if k != 'f'])
