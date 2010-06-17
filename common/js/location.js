d0dd5ebee87e3fe01d4c1eaa62f17d13c6744d7d
Daniel Yamins <dyamins@daniel-yaminss-macbook-pro.local>
2010-06-17 12:12:24 -0400

    timedate.js and location.js

---
+++ common/js/location.js
@@ -0,0 +1,29 @@
+var _ = require("underscore.js")._
+
+var SPACE_CODE_DIVISIONS = {'A': 'Area Code', 'B': 'Metropolitan Division', 'C': 'country', 'D': 'Census Division', 'I': 'Incoporated Place', 'L': 'State Legislative District -- Upper', 'O': 'Continent', 'Q': 'School District -- Secondary', 'S': 'State Abbreviation', 'T': 'Census Tract', 'V': 'Village', 'W': 'City', 'X': 'Undefined', 'Z': 'ZCTA5', 'a': 'Address', 'b': 'Combined Statistical Area', 'c': 'County', 'd': 'Public Use Microdata Area -- 1%', 'e': 'Public Use Microdata Area -- 5%', 'f': 'FIPS', 'g': 'Congressional District', 'i': 'Island', 'j': 'County Subdivision', 'k': 'School District -- Unified', 'l': 'State Legislative District -- Lower', 'm': 'Metropolitan/Micropolitan Statistical Area', 'n': 'New England City and Town Area', 'p': 'Postal Code', 'q': 'School District -- Elementary', 'r': 'Census Region', 's': 'State', 't': 'Town', 'u': 'Urban Area', 'v': 'Voting District', 'z': 'ZCTA3'}
+
+
+var phrase = function (l){
+
+    var Sarray = new Array();
+
+    for (i in  _.keys(l)){
+        var key = _.keys(l)[i]
+        if (key !== 'f'){
+	    Sarray.push(l[key])
+ 	    }
+ 	else{
+ 	    for (j in _.keys(l[key])){
+ 		var k = _.keys(l[key])[j]
+ 		Sarray.push(l[key] + " FIPS=" + l[key][k])
+		
+ 	    }
+ 	}
+
+     }
+
+     return Sarray.join(' ')
+ }
+ 
+
+  exports.spacephrase = spacephrase
--- common/js/timedate.js
+++ common/js/timedate.js
@@ -1,31 +1,5 @@
 var _ = require("underscore.js")._
 
-var SPACE_CODE_DIVISIONS = {'A': 'Area Code', 'B': 'Metropolitan Division', 'C': 'country', 'D': 'Census Division', 'I': 'Incoporated Place', 'L': 'State Legislative District -- Upper', 'O': 'Continent', 'Q': 'School District -- Secondary', 'S': 'State Abbreviation', 'T': 'Census Tract', 'V': 'Village', 'W': 'City', 'X': 'Undefined', 'Z': 'ZCTA5', 'a': 'Address', 'b': 'Combined Statistical Area', 'c': 'County', 'd': 'Public Use Microdata Area -- 1%', 'e': 'Public Use Microdata Area -- 5%', 'f': 'FIPS', 'g': 'Congressional District', 'i': 'Island', 'j': 'County Subdivision', 'k': 'School District -- Unified', 'l': 'State Legislative District -- Lower', 'm': 'Metropolitan/Micropolitan Statistical Area', 'n': 'New England City and Town Area', 'p': 'Postal Code', 'q': 'School District -- Elementary', 'r': 'Census Region', 's': 'State', 't': 'Town', 'u': 'Urban Area', 'v': 'Voting District', 'z': 'ZCTA3'}
-
-
-var spacephrase = function (l){
-
-    var Sarray = new Array();
-
-    for (i in  _.keys(l)){
-        var key = _.keys(l)[i]
-        if (key !== 'f'){
-	    Sarray.push(l[key])
- 	    }
- 	else{
- 	    for (j in _.keys(l[key])){
- 		var k = _.keys(l[key])[j]
- 		Sarray.push(l[key] + " FIPS=" + l[key][k])
-		
- 	    }
- 	}
-
-     }
-
-     return Sarray.join(' ')
- }
-
-
  var DEFAULT_T =  [['Y',0],['m',1],['d',1],['H',0],['M',0],['S',0],['MS',0]];
 
  var tObjFlatten = function(tObj){
@@ -122,7 +96,7 @@ var spacephrase = function (l){
   }
 
 
-      exports.spacephrase = spacephrase
-      exports.tObjFlatten = tObjFlatten;
-      exports.convertToDT = convertToDT;
-      exports.timephrase=timephrase;
+
+exports.tObjFlatten = tObjFlatten;
+exports.convertToDT = convertToDT;
+exports.timephrase=timephrase;
