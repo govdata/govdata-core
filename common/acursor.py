f064e74d8641367479fc017e45f6a4d1db64086c
Dan Yamins <dyamins@gmail.com>
2011-01-05 00:01:23 -0500

    test

--- common/acursor.py
+++ common/acursor.py
@@ -602,6 +602,7 @@ class asyncCursorHandler(tornado.web.RequestHandler):
             self.write('}')  
         
         if self.returnObj and not self.stream:
+            print(self.data)
             self.write(json.dumps(self.data,default=pm.json_util.default))
 
         if self.jsonPcallback:
