75ee81fbbd646535f329169c2659126cd6664177
Dan Yamins <dyamins@gmail.com>
2011-01-09 18:50:33 -0500

    attempt at bugfix

--- backbackend/govdata/deploy.py
+++ backbackend/govdata/deploy.py
@@ -242,7 +242,7 @@ def get_source_data(collectionName,connection =None):
     db = connection['govdata']
     collection = db['__COMPONENTS__']
     
-    X = collection.find_one({'name':collectionName})
+    X = collection.find_one({'name':collectionName}).items()
     
     if not X:
         raise NameNotVerifiedError(collectionName)
