00c2d5e66d821d7e38b5f7472390c68142f68750
Dan Yamins <dyamins@gmail.com>
2011-01-08 22:02:51 -0500

    test

--- backend/api.py
+++ backend/api.py
@@ -910,6 +910,8 @@ class sourceHandler(asyncCursorHandler):
         
         querySequence = getQuerySequence(args)
         
+        print("THING",querySequence)
+        
         querySequence = [[str(action),list(getArgs(args))] for (action,args) in querySequence]
     
         if querySequence[0][1][0] == () or not (querySequence[0][1][0][0].has_key('versionOffset') or querySequence[0][1][0][0].has_key('version')):
